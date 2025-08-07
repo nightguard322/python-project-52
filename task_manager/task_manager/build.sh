b1477de2d12a5503585e1d4f6bf3448a96ca3239
Your Name <you@example.com>
2025-08-07 22:26:00 +0300

    stared with setting up

---
+++ task_manager/task_manager/build.sh
@@ -0,0 +1,6 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
+# здесь добавьте все необходимые команды для установки вашего проекта
+# команду установки зависимостей, сборки статики, применения миграций и другие
+make install && make collectstatic && make migrate
