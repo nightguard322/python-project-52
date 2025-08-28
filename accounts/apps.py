24b0411988da8a2da3c6e0da332962c5e938af9d
Your Name <you@example.com>
2025-08-28 17:08:21 +0300

    added rollbar

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
-
+import os
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    import rollbar
+    rollbar.init(
+        access_token=os.getenv('ROLLBACK_ACCESS_TOKEN'),
+        environment=os.getenv('DJANGO_ENVIRONMENT'),
+        code_version=os.getenv('GIT_COMMIT')
+    )
+    rollbar.report_message('Rollbar is configured correctly', 'info')
+    rollbar.report_message("Тестовое сообщение из Django", 'info')
+        
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import rollbar
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = BASE_DIR / '.env'
+if env_path:
+    load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
