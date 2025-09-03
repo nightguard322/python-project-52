100532c0a974210e048ddf7827b7b516a8831d4b
Your Name <you@example.com>
2025-09-03 10:22:45 +0300

    config changes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
     'accounts',
     'crispy_forms',
     'crispy_bootstrap5',
