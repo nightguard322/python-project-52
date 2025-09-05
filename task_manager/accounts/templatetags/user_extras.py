bdecc7effb33805c781cb6b07f47ecfd71c3f5ee
Your Name <you@example.com>
2025-09-05 19:28:12 +0300

    fixed f-l names

--- task_manager/accounts/templatetags/user_extras.py
+++ task_manager/accounts/templatetags/user_extras.py
@@ -5,4 +5,4 @@ register = template.Library()
 @register.filter
 def full_name(user, default="None"):
     full_name = f"{user.first_name} {user.last_name}".strip()
-    return full_name or default
+    return full_name.title() or default
